22204f5c657275b9628123a0ae63786de6642bcd
danielvito-pereira <danielvitor081@gmail.com>
2022-11-29 13:01:23 -0300

    sections

---
+++ src/components/HeroSection.vue
@@ -0,0 +1,134 @@
+<template>
+  <section class="heroSection">
+    <div class="heading">
+      <div class="head">
+        <div class="text-block">
+          <h1 class="titulo">Get More Done with whitepace</h1>
+          <p class="paragrafo">
+            Project management software that enables your teams to collaborate, plan, analyze and manage everyday tasks
+          </p>
+        </div>
+        <a class="bt -azul">Try Whitepace free</a>
+      </div>
+      <div class="img"></div>
+    </div>
+  </section>
+</template>
+
+<style lang="scss">
+  .heroSection{
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    padding: 1em;
+    gap: 100px;
+    width: 100%;
+    height: 813px;
+    background: #043873;
+    background-image: url("../assets/img/Element.svg");
+    background-repeat: no-repeat;
+    background-size: cover;
+    .heading{
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      align-items: center;
+      padding: 0px;
+      gap: 50px;
+      height:90%;
+      min-width: 288px;
+      height: 653px;
+
+      .head{
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        padding: 1em;
+        gap: 60px;
+        width: 90%;
+        .text-block {
+          display: flex;
+          flex-direction: column;
+          justify-content: center;
+          align-items: center;
+          padding: 1em;
+          gap: 24px;
+          width: 100%;
+          h1.titulo{
+            font-family: 'Inter';
+            font-style: normal;
+            font-weight: 700;
+            font-size: 46px;
+            line-height: 44px;
+            text-align: center;
+            letter-spacing: -0.02em;
+            color:var(--white);
+          }
+          .paragrafo{
+            font-family: 'Inter';
+            font-style: normal;
+            font-weight: 400;
+            font-size: 18px;
+            line-height: 30px;
+            text-align: center;
+            letter-spacing: -0.02em;
+            color:var(--white);
+          }
+
+        }
+      }
+      .img{
+        display: flex;
+        flex-direction: column;
+        align-items: flex-start;
+        padding: 0px;
+        gap: 4px;
+        background-color: aqua;
+        width: 90%;
+        min-width: 270px;
+        height: 191px;
+      }
+    }
+  }
+
+/*Botão */
+  .bt{
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+    padding: 1.5em ;
+    width: 227px;
+    background: #4F9CF9;
+    color: #FFFFFF;
+    border-radius: 8px;
+    cursor: pointer;
+
+
+    &:hover{
+      color: #043873;
+      transition: all .5s ease-in-out;
+    }
+    
+    
+  }
+  .-azul {
+      &::after{
+      content: url("../assets/img/Group\ 212.png");    
+      display: block;
+      margin-left:1em;
+    }
+    &:hover:after {
+      transform:  translate(5px, 0%);
+      transition: all 0.5s ease-in-out;
+    }
+    }
+</style>
+
+<script>
+export default {
+  name: 'HeroSection',
+}
+</script>
---
+++ src/components/WorkManagement.vue
@@ -0,0 +1,130 @@
+<template>
+  <section class="WorkManagement">
+    <div class="content">
+      <div class="heading">
+        <div class="head">
+          <div class="text-block">
+            <h1 class="titulo">Project Management</h1>
+            <p class="paragrafo">
+              Images, videos, PDFs and audio files are supported. Create math expressions and diagrams directly from the app. Take photos with the mobile app and save them to a note.
+            </p>
+          </div>
+          <a class="bt -azul">Get Started</a>
+        </div>
+      <div class="img"></div>
+      </div>
+    </div>
+    <div class="content">
+      <div class="heading">
+        <div class="head">
+          <div class="text-block">
+            <h1 class="titulo">Work together</h1>
+            <p class="paragrafo">
+              With whitepace, share your notes with your colleagues and collaborate on them.
+              You can also publish a note to the internet and share the URL with others.
+
+            </p>
+          </div>
+          <a class="bt -azul">Try it now</a>
+        </div>
+      <div class="img"></div>
+      </div>
+    </div>
+  </section>
+
+</template>
+<style lang="scss">
+  .WorkManagement{
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    padding: 80px 16px;
+    gap: 100px;
+    width: 100%;
+    height: 1586.74px;
+    left: 0px;
+
+    .content{
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      align-items: center;
+      padding: 0px;
+      gap: 60px;
+      width: 90%;
+      height: 659px;
+      
+        .heading{
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        padding: 0px;
+        gap: 50px;
+        height:100%;
+        min-width: 288px;
+        height: 653px;
+ 
+        .head{
+
+          display: flex;
+          flex-direction: column;
+          justify-content: center;
+          align-items: center;
+          padding: 1em;
+          gap: 60px;
+          width: 100%;
+          .text-block {
+            background:red;
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+            padding: 1em;
+            gap: 24px;
+            width: 100%;
+            h1.titulo{
+              font-family: 'Inter';
+              font-style: normal;
+              font-weight: 700;
+              font-size: 35px;
+              line-height: 44px;
+              text-align: center;
+              letter-spacing: -0.02em;
+              color:var(--white);
+            }
+            .paragrafo{
+              font-family: 'Inter';
+              font-style: normal;
+              font-weight: 400;
+              font-size: 18px;
+              line-height: 30px;
+              text-align: center;
+              letter-spacing: -0.02em;
+              color:var(--white);
+            }
+
+          }
+        }
+        .img{
+          display: flex;
+          flex-direction: column;
+          align-items: flex-start;
+          padding: 0px;
+          gap: 4px;
+          background-color: aqua;
+          width: 80%;
+          min-width: 270px;
+          height: 191px;
+        }
+        }
+    }
+  }
+</style>
+
+<script>
+export default {
+  name: 'HeroSection',
+}
+</script>
