a3b291c1a45a3ccd0f88db03ddadd5f166ccd8a9
danielvito-pereira <danielvitor081@gmail.com>
2022-11-29 13:35:28 -0300

    imagens

--- src/App.vue
+++ src/App.vue
@@ -27,17 +27,34 @@ export default {
   --blue-2:#4F9CF9;
   --paragraph: hsl(210, 9%, 31%);
   --button2: #FFE492;
+  --negro: #393939;
 }
 body {
   font-family: "Inter", Arial, sans-serif;
   box-sizing: border-box;
 
 }
+
+
+/*
+  Con este reset vamos a resolver:
+    👉 Unificar el uso de Custom Properties
+    👉 Problemas de box-model más generales
+    👉 Problemas con imagenes, vídeos e iconos svg
+    👉 Problemas con tipografías y etiquetas input en formularios
+    👉 Unificar la tipografía de todas las etiquetas de una web
+*/
+
+/* Las adaptamos al modo oscuro */
 @media (prefers-color-scheme: dark) {
   :root {
     --negro: #ececec;
   }
 }
+/* Opcional */
+/* Configuramos si un usuario ha activado el modo alto contraste. (WD) */
+/* Opcional */
+/* Desactivamos los animations en el caso de que el usuario haya configurado el modo sin animation */
 @media (prefers-reduced-motion: reduce) {
   * {
     -webkit-animation: none !important;
@@ -46,6 +63,7 @@ body {
     transition: none !important;
   }
 }
+/* Reseteamos los margin y paddings de todas las etiquetas */
 * {
   margin: 0;
   padding: 0;
@@ -54,15 +72,21 @@ body {
           box-sizing: border-box;
   vertical-align: baseline;
 }
+
+/* Evitamos problemas con las imagenes */
 img, picture, video, iframe, figure {
   max-width: 100%;
   width: 100%;
   display: block;
+  /* opcional */
   -o-object-fit: cover;
      object-fit: cover;
+  /* opcional */
   -o-object-position: center center;
      object-position: center center;
 }
+
+/* Reseteamos los enlaces para funcionar como cajas... */
 a {
   display: block;
   text-decoration: none;
@@ -70,18 +94,22 @@ a {
   font-size: inherit;
 }
 
+/* ... excepto los que se encuentran en párrafos */
 p a {
   display: inline;
 }
 
+/* Quitamos los puntos de los <li> */
 li {
   list-style-type: none;
 }
 
+/* Configuramos anclas suaves */
 html {
   scroll-behavior: smooth;
 }
 
+/* Desactivamos estilos por defecto de las principales etiquetas de texto */
 h1, h2, h3, h4, h5, h6, p, span, a, strong, blockquote, i, b, u, em {
   font-size: 1em;
   font-weight: inherit;
@@ -90,11 +118,13 @@ h1, h2, h3, h4, h5, h6, p, span, a, strong, blockquote, i, b, u, em {
   color: inherit;
 }
 
+/* Evitamos problemas con los pseudoelementos de quotes */
 blockquote:before, blockquote:after, q:before, q:after {
   content: "";
   content: none;
 }
 
+/* Configuramos el texto que seleccionamos */
 ::-moz-selection {
   background-color: var(--negro);
   color: var(--blanco);
@@ -104,6 +134,7 @@ blockquote:before, blockquote:after, q:before, q:after {
   color: var(--blanco);
 }
 
+/* Nivelamos problemas de tipografías y colocación de formularios */
 form, input, textarea, select, button, label {
   font-family: inherit;
   font-size: inherit;
@@ -111,32 +142,52 @@ form, input, textarea, select, button, label {
       -ms-hyphens: auto;
           hyphens: auto;
   background-color: transparent;
+  color: inherit;
   display: block;
-
+  /* opcional 
   -webkit-appearance: none;
      -moz-appearance: none;
           appearance: none;
+          */
 }
 
+/* Reseteamos las tablas */
 table, tr, td {
   border-collapse: collapse;
   border-spacing: 0;
 }
+
+/* Evitamos problemas con los SVG */
 svg {
   width: 100%;
   display: block;
   fill: currentColor;
 }
+
+/* (Probándolo, no usar en producción) En el caso de añadir una  */
+/* p svg{
+  display: inline;
+  width: initial;
+} */
+/* Configuramos la tipografía para toda la web */
 body {
   min-height: 100vh;
   font-size: 100%;
   font-family: var(--tipo-principal);
-  font-smooth: always;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+  color: var(--negro);
+  /* opcional */
+  line-height: 1.4em;
+  /* opcional */
+  -webkit-hyphens: auto;
+      -ms-hyphens: auto;
+          hyphens: auto;
+  /* opcional
+  font-smooth: always; */
+  /* opcional 
+  -webkit-font-smoothing: antialiased;*/
+  /* opcional 
+  -moz-osx-font-smoothing: grayscale;*/
 }
-
-
 
 
 
